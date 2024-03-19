00bdb65ce86251d6e78868793cc272fc1eb49022
minho.kim <alsgh5016@gmail.com>
2024-03-19 13:30:31 +0900

    Update custom code

--- custom/src/tutorial01.ml
+++ custom/src/tutorial01.ml
@@ -5,54 +5,37 @@ let print_instructions_in_blocks b =
     Printf.printf "      %s\n" (Llvm.string_of_llvalue i)
     ) b
 
-(* let print_cfg_of_function f = 
-  Printf.printf "CFG for function %s:\n" (value_name f);
-  Llvm.iter_blocks (fun b ->
-    match Llvm.block_terminator b with
-    | Some terminator ->
-      let successors = Llvm.successors terminator in
-      Printf.printf "  Block %s has successors: " (Llvm.value_name (Llvm.value_of_block b));
-      Array.iter (fun succ ->
-        Printf.printf "    %s " (Llvm.value_name (Llvm.value_of_block succ));
-        print_endline "\n      Successor instructions:";
-        Llvm.iter_instrs (fun i ->
-          Printf.printf "        %s\n" (Llvm.string_of_llvalue i)) succ
-          ) successors;
-      print_endline ""
-    | None -> Printf.printf "  Block %s has no terminator\n" (Llvm.value_name (Llvm.value_of_block b))
-    ) f *)
-
 let print_cfg_of_function f =
-  Printf.printf "CFG for function %s:\n" (value_name f);
+  Printf.printf "CFG for function %s:\n" (Llvm.value_name f);
+  (* 함수 내의 기본 블록들을 순회하며 번호 매기기 *)
+  let blocks = Llvm.fold_left_blocks (fun (acc, idx) b -> ((b, idx) :: acc, idx + 1)) ([], 0) f in
+  let blocks = List.rev (fst blocks) in  (* 순서대로 번호를 매기기 위해 리스트를 뒤집습니다. *)
   Llvm.iter_blocks (fun b ->
-    let b_name = match Llvm.value_name (Llvm.value_of_block b) with
-      | "" -> "Unnamed Block"
-      | name -> name in
+    let b_index = List.assoc b blocks in
+    let b_name = Printf.sprintf "block %d:" b_index in
+    Printf.printf "%s\n" b_name;
+    (* 여기에 각 블록의 IR 지시어를 출력하는 코드 추가 *)
+    Llvm.iter_instrs (fun i ->
+      Printf.printf "  %s\n" (Llvm.string_of_llvalue i)
+    ) b;
     match Llvm.block_terminator b with
     | Some terminator ->
       let successors = Llvm.successors terminator in
       if Array.length successors > 0 then (
-        Printf.printf "  %s -> (" b_name;
+        Printf.printf "  %s has successors {" b_name;
         Array.iter (fun succ ->
-          let succ_name = match Llvm.value_name (Llvm.value_of_block succ) with
-            | "" -> "Unnamed Succ"
-            | name -> name in
-          Printf.printf "  %s;" succ_name
-          ) successors;
-          Printf.printf " }\n";
-          Array.iter (fun succ ->
-            Printf.printf "    Successor %s instructions:\n" (match Llvm.value_name (Llvm.value_of_block succ) with
-            | "" -> "Unnamed Block"
-            | name -> name);
-            print_instructions_in_blocks succ
-            ) successors;
+          let succ_index = List.assoc succ blocks in
+          let succ_name = Printf.sprintf "block %d" succ_index in
+          Printf.printf " %s;" succ_name
+        ) successors;
+        Printf.printf " }\n";
       ) else (
-        Printf.printf "    %s has no successors\n" b_name;
+        Printf.printf "  %s has no successors\n" b_name;
       )
-    | None -> Printf.printf "    %s has no terminator\n" b_name 
-    ) f;
-    print_endline ""
-
+    | None -> Printf.printf "  %s has no terminator\n" b_name 
+  ) f;
+  print_endline ""
+    
 
 let print_instructions m =
   iter_functions (fun f ->
